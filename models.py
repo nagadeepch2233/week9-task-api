e17a60275a065053b69616416ac0b50bedfa1b17
nagadeepch2233 <nagadeepch@gmail.com>
2026-03-09 19:22:34 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    tasks = db.relationship("Task", backref="owner", lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    status = db.Column(db.String(20), default="pending")
+    priority = db.Column(db.Integer, default=1)
+    due_date = db.Column(db.DateTime)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
